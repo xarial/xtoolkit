cd8c0938c3c9a4d38e680e1c331a0b2b76b4af2a
Artem <artem.taturevych@xarial.com>
2020-04-20 19:13:07 +1000

    Renamed namespaces for some reflection classes
    Added BindingProxy
    Added image to image source converter and extension method
    Added assembly extension to load wpf resources

---
+++ src/Wpf/Extensions/ImageExtension.cs
@@ -0,0 +1,37 @@
+﻿//*********************************************************************
+//xToolkit
+//Copyright(C) 2020 Xarial Pty Limited
+//Product URL: https://xtoolkit.xarial.com
+//License: https://xtoolkit.xarial.com/license/
+//*********************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+using System.Windows.Media.Imaging;
+
+namespace Xarial.XToolkit.Wpf.Extensions
+{
+    public static class ImageExtension
+    {
+        public static BitmapImage ToBitmapImage(this Image img)
+        {
+            using (var stream = new MemoryStream())
+            {
+                img.Save(stream, ImageFormat.Png);
+                stream.Seek(0, SeekOrigin.Begin);
+
+                var bmpImg = new BitmapImage();
+                bmpImg.BeginInit();
+                bmpImg.CacheOption = BitmapCacheOption.OnLoad;
+                bmpImg.StreamSource = stream;
+                bmpImg.EndInit();
+
+                return bmpImg;
+            }
+        }
+    }
+}
