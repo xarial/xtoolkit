c1d6a50c4e2855fda06d3196a8810e11da39ec1f
Artem <artem.taturevych@xarial.com>
2021-01-22 14:56:03 +1100

    Modified local folder binding redirect to have a match filter

--- src/Utils/Reflection/LocalFolderReferencesResolver.cs
+++ src/Utils/Reflection/LocalFolderReferencesResolver.cs
@@ -5,12 +5,25 @@ using System.Text;
 
 namespace Xarial.XToolkit.Reflection
 {
+    [Flags]
+    public enum AssemblyMatchFilter_e 
+    {
+        PublicKeyToken = 1,
+        Culture = 2,
+        Version = 4
+    }
+
     public class LocalFolderReferencesResolver : AssemblyNameReferenceResolver
     {
         private readonly string m_SearchDir;
 
-        public LocalFolderReferencesResolver(string searchDir)
+        private readonly AssemblyMatchFilter_e m_MatchFilter;
+        
+        public LocalFolderReferencesResolver(string searchDir,
+            AssemblyMatchFilter_e matchFilter = AssemblyMatchFilter_e.PublicKeyToken | AssemblyMatchFilter_e.Culture)
         {
+            m_MatchFilter = matchFilter;
+
             m_SearchDir = searchDir;
         }
 
@@ -20,6 +33,14 @@ namespace Xarial.XToolkit.Reflection
             searchDir = m_SearchDir;
             recursiveSearch = true;
             return assmName;
+        }
+
+        protected override bool Match(AssemblyName probeAssmName, AssemblyName searchAssmName)
+        {
+            return (probeAssmName.Name == searchAssmName.Name)
+                && (!m_MatchFilter.HasFlag(AssemblyMatchFilter_e.PublicKeyToken) || GetPublicKeyToken(probeAssmName) == GetPublicKeyToken(searchAssmName))
+                && (!m_MatchFilter.HasFlag(AssemblyMatchFilter_e.Culture) || probeAssmName.CultureName == probeAssmName.CultureName)
+                && (!m_MatchFilter.HasFlag(AssemblyMatchFilter_e.Version) || probeAssmName.Version == searchAssmName.Version);
         }
     }
 }
