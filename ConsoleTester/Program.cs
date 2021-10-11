da18462b14b4dd5f8591f8b2434ea6a821ee252e
Artem <artem.taturevych@xarial.com>
2021-10-12 09:51:13 +1100

    Updated pipeline to publish public preview feed
    Added console tester project

---
+++ ConsoleTester/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xarial.XToolkit.Reflection;
+
+namespace ConsoleTester
+{
+    class Program
+    {
+        static Program() 
+        {
+            AppDomain.CurrentDomain.ResolveBindingRedirects(new LocalFolderReferencesResolver(
+                Path.GetDirectoryName(typeof(Program).Assembly.Location),
+                AssemblyMatchFilter_e.PublicKeyToken | AssemblyMatchFilter_e.Culture));
+        }
+
+        static void Main(string[] args)
+        {
+            Foo();
+        }
+        
+        static void Foo() 
+        {
+            var f = new Lib.SampleClass();
+            f.Foo();
+        }
+    }
+}
