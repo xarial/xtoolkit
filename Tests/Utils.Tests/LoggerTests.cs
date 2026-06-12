6558ee5949d023ce27dcb8772b0dc5b27aa3b24f
Artem Taturevych <artem.taturevych@xarial.com>
2026-06-12 13:17:00 +1000

    Added logger

---
+++ Tests/Utils.Tests/LoggerTests.cs
@@ -0,0 +1,107 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xarial.XToolkit.Reporting;
+
+namespace Utils.Tests
+{
+    public class LoggerTests
+    {
+        private class LoggerMock : TraceLogger
+        {
+            private readonly List<string> m_Msgs;
+
+            public LoggerMock(List<string> msgs, string category, bool singleLine = true) : base(category, singleLine)
+            {
+                m_Msgs = msgs;
+            }
+
+            protected override void WriteLine(string line) => m_Msgs.Add(line);
+        }
+
+        [Test]
+        public void TestLogSingleLineNoStackTrace() 
+        {
+            var ex = new Exception("Test1", new Exception("Test2", new Exception("Test3")));
+            
+            var msgs = new List<string>();
+
+            var logger = new LoggerMock(msgs, "Test");
+
+            logger.Log(ex, false);
+
+            Assert.AreEqual(3, msgs.Count);
+            Assert.AreEqual("Test1", msgs[0]);
+            Assert.AreEqual("Test2", msgs[1]);
+            Assert.AreEqual("Test3", msgs[2]);
+        }
+
+        [Test]
+        public void TestLogSingleLineStackTrace()
+        {
+            Exception ex;
+
+            try
+            {
+                throw new Exception("Test1", new Exception("Test2", new Exception("Test3")));
+            }
+            catch (Exception e)
+            {
+                ex = e;
+            }
+
+            var msgs = new List<string>();
+
+            var logger = new LoggerMock(msgs, "Test");
+
+            logger.Log(ex, true);
+
+            var frame = new StackFrame(0, true);
+            var method = frame.GetMethod();
+
+            var stackTrace = $"   at {method.DeclaringType.FullName}.{method.Name}() in {frame.GetFileName()}";
+
+            Assert.AreEqual(4, msgs.Count);
+            Assert.AreEqual("Test1", msgs[0]);
+            Assert.That(msgs[1].StartsWith(stackTrace));
+            Assert.AreEqual("Test2", msgs[2]);
+            Assert.AreEqual("Test3", msgs[3]);
+        }
+
+        [Test]
+        public void TestLogMultiLineStackTrace()
+        {
+            Exception ex;
+
+            try
+            {
+                throw new Exception("Test1", new Exception("Test2", new Exception("Test3")));
+            }
+            catch (Exception e)
+            {
+                ex = e;
+            }
+
+            var msgs = new List<string>();
+
+            var logger = new LoggerMock(msgs, "Test", false);
+
+            logger.Log(ex, true);
+
+            var frame = new StackFrame(0, true);
+            var method = frame.GetMethod();
+
+            var stackTrace = $"   at {method.DeclaringType.FullName}.{method.Name}() in {frame.GetFileName()}";
+
+            Assert.AreEqual(1, msgs.Count);
+            Assert.That(msgs[0].Contains("Test1"));
+            Assert.That(msgs[0].Contains("Test2"));
+            Assert.That(msgs[0].Contains("Test3"));
+            Assert.That(msgs[0].Contains(stackTrace));
+        }
+    }
+}
---
+++ src/Utils/Reporting/Logger.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Xarial.XToolkit.Reporting
+{
+    /// <summary>
+    /// Message logger
+    /// </summary>
+    public interface ILogger
+    {
+        /// <summary>
+        /// Logs message
+        /// </summary>
+        /// <param name="msg">Content to log</param>
+        void Log(string msg);
+    }
+
+    /// <summary>
+    /// Simple logger to output messages to trace window
+    /// </summary>
+    public class TraceLogger : ILogger
+    {
+        private readonly string m_Category;
+        private readonly bool m_SingleLine;
+
+        private readonly Regex m_SplitLineRegex;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="category">Trace category</param>
+        /// <param name="singleLine">Force single line</param>
+        public TraceLogger(string category, bool singleLine = true) 
+        {
+            m_Category = category;
+            m_SingleLine = singleLine;
+
+            m_SplitLineRegex = new Regex(@"\r\n?|\n", RegexOptions.Compiled);
+        }
+
+        /// <inheritdoc/>
+        public void Log(string msg)
+        {
+            if (!string.IsNullOrEmpty(msg))
+            {
+                if (m_SingleLine)
+                {
+                    foreach (var line in m_SplitLineRegex.Split(msg))
+                    {
+                        if (!string.IsNullOrEmpty(line))
+                        {
+                            WriteLine(line);
+                        }
+                    }
+                }
+                else
+                {
+                    WriteLine(msg);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Write line to trace
+        /// </summary>
+        /// <param name="line">Line to write</param>
+        protected virtual void WriteLine(string line) => System.Diagnostics.Trace.WriteLine(line, m_Category);
+    }
+
+    /// <summary>
+    /// Additional methods of <see cref="TraceLogger"/>
+    /// </summary>
+    public static class LoggerExtension
+    {
+        /// <summary>
+        /// Logs exception to log
+        /// </summary>
+        /// <param name="logger">Logger</param>
+        /// <param name="ex">Exception to log</param>
+        /// <param name="logCallStack">True to log stack trace</param>
+        public static void Log(this ILogger logger, Exception ex, bool logCallStack = true)
+        {
+            var exContent = new StringBuilder();
+
+            foreach (var line in ParseException(ex, logCallStack)) 
+            {
+                exContent.AppendLine(line);
+            }
+
+            logger.Log(exContent.ToString());
+        }
+
+        private static IEnumerable<string> ParseException(Exception ex, bool logCallStack)
+        {
+            if (ex != null)
+            {
+                yield return ex.Message;
+
+                if (logCallStack)
+                {
+                    var stackTrace = ex.StackTrace;
+
+                    if (!string.IsNullOrEmpty(stackTrace)) 
+                    {
+                        yield return stackTrace;
+                    }
+                }
+
+                if (ex.InnerException != null)
+                {
+                    foreach (var log in ParseException(ex.InnerException, logCallStack))
+                    {
+                        yield return log;
+                    }
+                }
+            }
+        }
+    }
+}
